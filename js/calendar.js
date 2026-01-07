16af407f9d7e28ba3cdd61e62ae80661a6ce30bf
Tatiana Golub <tatiana.k1989@gmail.com>
2026-01-07 17:25:18 +0200

    feat: add ability to open only current and previous date

--- js/calendar.js
+++ js/calendar.js
@@ -2,11 +2,32 @@ import { openModal } from "./modal.js";
 import { wishes } from "./data.js";
 
 export function initCalendar() {
+  const today = getTodayAdventDay();
+
   document.addEventListener("click", (e) => {
-  if (e.target.tagName === "AREA") {
-    const day = e.target.dataset.day;
-    openModal(`Day ${day}`, wishes[day]);
-  }
-});
+    if (e.target.tagName !== "AREA") return;
+
+    const day = Number(e.target.dataset.day);
+
+    if (day <= today) {
+      openModal(`Day ${day}`, wishes[day]);
+    } else {
+      openModal(
+        `Day ${day}`,
+        "Too early. This window will open soon."
+      );
+    }
+  });
 }
 
+function getTodayAdventDay() {
+  const now = new Date();
+  // const month = now.getMonth(); // Use this constant if you wish the calendar windows to open only in December
+  // if (month !== 11) return 0; 
+
+  const day = now.getDate();
+
+  if (day > 24) return 24;
+
+  return day;
+}
